ac4f5913d2b313e3b9786c4a6dbe4eaba457a3a2
GuilhermeAntunes <guilherme.antunes.silva@hotmail.com>
2025-05-16 15:32:41 -0300

    Atualização final code e README

--- Jsonplaceholder.py
+++ Jsonplaceholder.py
@@ -2,167 +2,250 @@ import requests
 import json
 import time
 
+all_posts = []
 
-def new_post(titulo, texto, id):
-
-    url = 'https://jsonplaceholder.typicode.com/posts/'
-    body = {
-        'title': titulo,
-        'body': texto,
-        'userId' : id
-    }
-
-    try:
-        response = requests.post(url, body)
-        print(response.status_code)
-
-        if response.status_code == 201:
-            data = response.json()
-            return data
+def get_post(id):
+    """
+    Obtém os posts públicos de um usuário pelo ID usando a API JSONPlaceholder.
     
-    except requests.exceptions.RequestException as e:
-        print('Erro em new post', e)
+    Parâmetros:
+        id (int): ID do usuário (1 a 100).
     
-    
-def get_post(url, id):
-    url = f'https://jsonplaceholder.typicode.com/posts/{id}'
-    response = requests.get(url)
-    print(response.status_code)
+    Retorna:
+        list: Lista de posts do usuário, se encontrados.
+    """
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    params = {'userId': id}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
     if response.status_code == 200:
         data = response.json()
-        print(data)
-
-def new_user(usuarios):
-
-    email = input('Digite seu email: ')
-
-    for u in usuarios:
-        if u['email'] == email:
-            print('Email ja cadastrado')
-            return None
-        
-    senha = input('Digite sua senha: ')
-
-    user = {
-            'id': len(usuarios)+1,
-            'email' : email,
-            'senha': senha,
-            'posts': [],
-            'interacoes': []
-            }
-    
-    usuarios.append(user)
-    print('Usuário cadastrado com sucesso!')
-
-def login(usuarios):
-    email = input('Digiete seu email: ')
-    senha = input('Digite sua senha: ')
-    for u in usuarios:
-        if u['email'] == email and u['senha'] == senha:
-            return u
-        
+        return data
     else:
-        print('Email ou Senha incorretos')
+        print(f'Erro de requisição! Status code: {response.status_code}')
+
+
+def new_post(id, title, text):
+    """
+    Cria um novo post para o usuário na API JSONPlaceholder.
+    
+    Parâmetros:
+        id (int): ID do usuário.
+        title (str): Título do post.
+        text (str): Conteúdo do post.
+    
+    Retorna:
+        dict: Dados do post criado.
+    """
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    body = {
+        "userId": id,
+        "title": title,
+        "body": text
+    }
+
+    response = requests.post(url, json=body)
+
+    if response.status_code == 201:
+        data = response.json()
+        return data
+    else:
+        print(f'Erro de requisição! Status code: {response.status_code}')
+
 
 def menu_login():
+    """
+    Exibe o menu de login e retorna a opção escolhida pelo usuário.
     
-        print('1 - Cadastrar\n' \
-              '2 - Logar\n' \
-              '3 - Sair')
-        opcao = input('Digite a opção que deseja: ')
+    Retorna:
+        str: Opção escolhida.
+    """
+    print('-=' * 25)
+    print("1 - Cadastrar\n"
+          "2 - Entrar\n"
+          "3 - Sair\n")
+    opcao = input('Digite a opção: ')
+    return opcao
 
-        return opcao
 
-def menu(todos_posts, user):
-    print('1 - Visualizar Posts\n' \
-          '2 - Visualizar meus Posts\n' \
-          '3 - Filtrar posts por User\n' \
-          '4 - Fazer novo post\n' \
-          '5 - Sair')
+def menu(all_posts, user):
+    """
+    Exibe o menu principal após login e gerencia as ações do usuário.
+    
+    Parâmetros:
+        all_posts (list): Lista com todos os posts criados.
+        user (dict): Dados do usuário logado.
+    """
     while True:
-        try:
-            opcao = int(input('Digite a opção que deseja: '))
-            if opcao not in range(1,6):
-                print('Digite um numero valido [1-5]')
-                continue
+        print('-=' * 25)
+        print("1 - Visualizar posts\n"
+              "2 - Fazer post\n"
+              "3 - Visualizar meus posts\n"
+              "4 - Sair\n")
 
-            if opcao == 1:
-                print(todos_posts)
-            elif opcao == 2:
-                if user['posts']:
-                    print('\n---- Meus Posts ----')
-                    for post in user['post']:
-                        print(post)
-                user['interacoes'].append("Visualizou todos os posts")
-            
-            elif opcao == 3:
+        try:
+            opcao = input('Digite a opção: ')
+
+            if opcao == '1':
                 while True:
                     try:
-                        id = int(input('Digite o numero de Id do user que deseja ver [1-100]: '))
-                        if id in range(1,101):
-                            get_post(id)
-                            user['interacoes'].append("Visualizou posts por user")
+                        id = int(input('Digite o número do ID do usuário que deseja ver os posts [1-100]: '))
+                        if 1 <= id <= 100:
+                            data = get_post(id)
+
+                            for p in data:
+                                print(f"UserId: {p['userId']}")
+                                print(f"Título: {p['title']}")
+                                print(f"Texto: {p['body']}")
+                                print('~-' * 25)
+
+                            for post in all_posts:
+                                if post['userId'] == id:
+                                    print(f"UserId: {post['userId']}")
+                                    print(f"Título: {post['title']}")
+                                    print(f"Texto: {post['body']}")
+                                    print('~-' * 25)
+
+                            user['interacoes'].append('Visualizou todos os posts')
                             break
                         else:
-                            print('Digite um valor valido [1-100]')
-                            continue
-
+                            print('Número inválido, digite um número entre 1 e 100.')
                     except ValueError:
-                        print('Digite um valor valido [1-100]')
-
-            elif opcao == 4:
-                print('\n--- Novo Post ---')
-                titulo = input('Digite um Titulo para seu post: ')
-                texto = input('Digite seu texto: ')
-                id_user = user['id']
-                new_post(titulo, texto, id_user)
-
-                user['interacoes'].append("Fez um novo post")
-            
-            elif opcao == 5:
-                print('Saindo...')
-                time.sleep(2)
-
-
-        except ValueError:
-            print('Digite um numero valido [1-5]')
-        
-        except KeyboardInterrupt:
-            print('Programa finalizado pelo usuário')
-            exit()
-        
-def main():
-    usuarios = []
-    todos_posts = []
-    while True:
-        opcao = menu_login()
-        try:
-            if opcao == '1':
-                new_user(usuarios)
-
+                        print('Número inválido, digite um número inteiro.')
+                
             elif opcao == '2':
-                novo_login = login(usuarios)
-                if novo_login:
-                    menu(todos_posts, novo_login)
+                new_title = input('Digite um título para seu post: ')
+                new_text = input('Digite o texto: ')
+                userid = user['userId']
+
+                user_post = new_post(userid, new_title, new_text)
+                all_posts.append(user_post)
+                user['posts'].append(user_post)
+                user['interacoes'].append('Fez um post')
+                print('Publicando...')
+                time.sleep(1)
+                print('Seu post foi publicado com sucesso!')
 
             elif opcao == '3':
-                print()
+                if not user['posts']:
+                    print('Nenhum post encontrado.')
+                    continue
+
+                print("Meus posts".center(50, '-'))
+                for p in user['posts']:
+                    print()
+                    print(f"UserId: {p['userId']}")
+                    print(f"Título: {p['title']}")
+                    print(f"Texto: {p['body']}")
+                    print('~-' * 25)
+
+                user['interacoes'].append('Visualizou seus posts')
+
+            elif opcao == '4':
+                if user['interacoes']:
+                    print("Interações no Sistema".center(50, '-'))
+                    for i in user['interacoes']:
+                        print(i)
+                print('-~' * 25)
                 print('Saindo...')
                 time.sleep(2)
-                print('Até mais!!')
+                break
+
+            else:
+                print('Opção inválida!')
+
+        except ValueError:
+            print('Valor inválido!')
+        except KeyboardInterrupt:
+            print('\nPrograma finalizado pelo usuário.')
+            exit()
+
+
+def cadastro(user_list):
+    """
+    Cadastra um novo usuário, garantindo que o e-mail não esteja repetido.
+    
+    Parâmetros:
+        user_list (list): Lista de usuários cadastrados.
+    """
+    print('~-' * 25)
+    email = input('Digite seu e-mail: ')
+
+    for user in user_list:
+        if user['email'] == email:
+            print('E-mail já cadastrado!')
+            return None
+
+    senha = input('Digite sua senha: ')
+    print('~-' * 25)
+
+    new_user = {
+        'userId': len(user_list) + 1,
+        'email': email,
+        'senha': senha,
+        'posts': [],
+        'interacoes': []
+    }
+    user_list.append(new_user)
+    print('Usuário cadastrado com sucesso!')
+
+
+def login(user_list):
+    """
+    Realiza o login do usuário com base no e-mail e senha.
+    
+    Parâmetros:
+        user_list (list): Lista de usuários cadastrados.
+    
+    Retorna:
+        dict or None: Usuário autenticado ou None.
+    """
+    print('-=' * 25)
+    email = input('Digite seu e-mail: ')
+    senha = input('Digite sua senha: ')
+    print('-=' * 25)
+
+    for user in user_list:
+        if user['email'] == email and user['senha'] == senha:
+            print(f'Bem-vindo, {email}!')
+            return user
+
+    print('E-mail ou senha inválidos!')
+    return None
+
+
+def main():
+    """
+    Função principal do sistema. Gerencia o fluxo de cadastro, login e menu principal.
+    """
+    user_list = []
+
+    while True:
+        opcao = menu_login()
+
+        try:
+            if opcao == '1':
+                cadastro(user_list)
+
+            elif opcao == '2':
+                new_user = login(user_list)
+                if new_user:
+                    menu(all_posts, new_user)
+
+            elif opcao == '3':
+                print('Saindo...')
+                time.sleep(1.5)
+                print('Até mais!')
                 exit()
 
             else:
-                print('A opção escolhida não é valida')
-                continue
+                print('Opção inválida!')
 
-        except ValueError:
-                print('A opção escolhida não é valida')
         except KeyboardInterrupt:
-            print('Programa finalizado pelo usuário')
+            print('\nPrograma finalizado pelo usuário.')
             exit()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
---
+++ README.dm
@@ -0,0 +1,26 @@
+# Sistema de Postagens com JSONPlaceholder
+
+Este é um projeto simples em Python que simula um sistema de postagens de usuários. Utiliza a API pública [JSONPlaceholder](https://jsonplaceholder.typicode.com/) para buscar e enviar posts de forma fictícia. Inclui funcionalidades de cadastro, login e histórico de interações.
+
+## 🚀 Funcionalidades
+
+- Cadastro de novos usuários
+- Login com autenticação por e-mail e senha
+- Visualização de posts públicos por ID de usuário
+- Criação de novos posts (simulados via API)
+- Visualização dos próprios posts
+- Histórico de interações do usuário com o sistema
+
+## 🛠️ Tecnologias Utilizadas
+
+- Python 3.x
+- Biblioteca `requests` (para requisições HTTP)
+- Biblioteca `time` (para simulações com delays)
+- API: [JSONPlaceholder](https://jsonplaceholder.typicode.com/)
+
+## 📦 Como executar
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/seu-usuario/nome-do-repositorio.git
+   cd nome-do-repositorio
